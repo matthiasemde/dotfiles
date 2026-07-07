686d703b14742455cd492381763ecd1f2ae503d8
Matthias Emde <matthias.emde@mvtec.com>
2026-07-07 15:23:44 +0200

    feat: add groovy lint tool

--- common/tools/default.nix
+++ common/tools/default.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   pkgs,
   username,
   email,
@@ -18,10 +19,32 @@ let
   txt2htmlPreview = pkgs.writeShellScriptBin "txt2htmlPreview" (
     builtins.readFile ./txt2htmlPreview.sh
   );
+
+  groovy-lint = pkgs.buildNpmPackage (finalAttrs: {
+    pname = "npm-groovy-lint";
+    version = "17.0.5";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "nvuillam";
+      repo = "npm-groovy-lint";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-Cq4SPOqR2mb2Foc1jlrA6B7qJBcmgLfcC84iTc4+tcw=";
+    };
+
+    npmDepsHash = "sha256-XGXiuqA0JmuFVretXDjWejV9HJAK6eWR9/LR3rUI99s=";
+
+    meta = with lib; {
+      description = "Lint, format and auto-fix Groovy / Jenkinsfile";
+      homepage = "https://github.com/nvuillam/npm-groovy-lint";
+      license = licenses.mit; # check upstream
+      platforms = platforms.all;
+    };
+  });
 in
 {
   home.packages = [
     gpgHelper
     txt2htmlPreview
+    groovy-lint
   ];
 }
