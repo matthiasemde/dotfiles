bff6508bfc2f86754cece9991d6104119bf2a685
Matthias Emde <matthias@emdemail.de>
2026-07-12 15:07:22 +0200

    feat: expand vicinae config

--- common/desktop/window-manager/vicinae.nix
+++ common/desktop/window-manager/vicinae.nix
@@ -8,5 +8,13 @@
   config = lib.mkIf config.dotfiles.desktop.enable {
     programs.vicinae.enable = true;
     programs.vicinae.systemd.enable = true;
+    programs.vicinae.enableFirefoxIntegration = true;
+    programs.vicinae.settings = {
+      launcher_window = {
+        opacity = 1.0;
+        compact_mode.enabled = true;
+      };
+      favorites = [ ];
+    };
   };
 }
