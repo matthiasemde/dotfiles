b44208f9f9b9ad9ca629348ea1e0e0ab68762b9b
Matthias Emde <matthias@emdemail.de>
2026-07-12 15:04:23 +0200

    feat: include element via home-manager options

--- common/desktop/default.nix
+++ common/desktop/default.nix
@@ -18,10 +18,32 @@
     home.packages = with pkgs; [
       feishin
       signal-desktop
-      element-desktop
       nerd-fonts.iosevka
       keymapp
       vlc
     ];
+
+    programs.element-desktop = {
+      enable = true;
+      settings = {
+        default_server_config = {
+          "m.homeserver" = {
+            base_url = "https://matrix.emdecloud.de";
+            server_name = "emdecloud.de";
+          };
+          "m.identity_server" = {
+            base_url = "https://vector.im";
+          };
+        };
+        disable_custom_urls = false;
+        disable_guests = false;
+        disable_login_language_selector = false;
+        disable_3pid_login = false;
+        force_verification = false;
+        brand = "Element";
+        integrations_ui_url = "https://scalar.vector.im/";
+        integrations_rest_url = "https://scalar.vector.im/api";
+      };
+    };
   };
 }
